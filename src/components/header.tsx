85f9f11ae9beacbbb8f0483b76bf3e14ca5380a9
Sandalu Thushan <sandaluthushan20@gmail.com>
2025-07-21 18:31:54 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Header>
    
    with the

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -34,6 +34,7 @@ const dynamicPageInfo: { [key: string]: (params: any) => { title: string; descri
   '^/weddings/booking/([^/]+)$': (params) => ({ title: `Booking #${params[0]}`, description: 'Details for wedding booking' }),
   '^/restaurant/menu/([^/]+)$': (params) => ({ title: `Restaurant & Dining Management`, description: 'Manage dining venues, menu items, and reservations' }),
   '^/restaurant/reservations/([^/]+)$': (params) => ({ title: 'Restaurant & Dining', description: `Details for reservation #${params[0]}` }),
+  '^/experience/([^/]+)/bookings/new$': (params) => ({ title: 'Experience Management', description: 'Create a new booking for this experience.' }),
   '^/experience/([^/]+)$': (params) => {
     const title = params[0].replace(/-/g, ' ').replace(/\b\w/g, (l: string) => l.toUpperCase());
     return { title: 'Experience Management', description: title };
