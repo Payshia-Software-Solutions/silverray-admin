b2a0477e5778c57e3061b40d0a1a4a898d83fdf9
Sandalu Thushan <sandaluthushan20@gmail.com>
2025-08-04 17:32:57 +0000

    i need to add CRUD operations for Create New Booking page this is my pos

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -18,6 +18,7 @@ const navItems = [
   { href: '/rooms', label: 'Room Management', icon: BedDouble },
   { href: '/amenities', label: 'Amenities Management', icon: Shield },
   { href: '/reservations', label: 'Room Booking Management', icon: CalendarCheck },
+  { href: '/bookings', label: 'Bookings', icon: ClipboardList },
   { href: '/restaurant', label: 'Restaurant & Dining', icon: UtensilsCrossed },
   { href: '/experience', label: 'Experience Management', icon: Star },
   { href: '/customers', label: 'Customer Management', icon: Users },
