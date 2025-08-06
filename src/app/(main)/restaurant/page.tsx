b038fbe895e9552fb282aeed42cf4405d0e903cf
Sandalu Thushan <sandaluthushan20@gmail.com>
2025-08-06 09:26:37 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.button

--- src/app/(main)/restaurant/page.tsx
+++ src/app/(main)/restaurant/page.tsx
@@ -217,7 +217,7 @@ export default function RestaurantDiningPage() {
               </TabsTrigger>
               <TabsTrigger value="features">
                 <Shield className="mr-2 h-4 w-4" />
-                Features Management
+                Restaurant Features
               </TabsTrigger>
             </TabsList>
             {activeTab === 'dining-venues' && (
