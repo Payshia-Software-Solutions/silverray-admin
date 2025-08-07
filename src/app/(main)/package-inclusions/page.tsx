3f1f695d0be7664e2f7deac3880b9d390e33ce57
Sandalu Thushan <sandaluthushan20@gmail.com>
2025-08-07 16:22:45 +0000

    This is my jason format file            "inclusion_type": "Diversity Tra

--- src/app/(main)/package-inclusions/page.tsx
+++ src/app/(main)/package-inclusions/page.tsx
@@ -88,6 +88,7 @@ export default function PackageInclusionsPage() {
               <Table>
                 <TableHeader>
                   <TableRow>
+                    <TableHead>Inclusion ID</TableHead>
                     <TableHead>Inclusion Type</TableHead>
                     <TableHead>Description</TableHead>
                     <TableHead className="text-right">Actions</TableHead>
@@ -96,6 +97,7 @@ export default function PackageInclusionsPage() {
                 <TableBody>
                   {inclusions.map((item) => (
                     <TableRow key={item.id}>
+                      <TableCell>{item.inclusion_id}</TableCell>
                       <TableCell className="font-medium">{item.inclusion_type}</TableCell>
                       <TableCell>{item.description}</TableCell>
                       <TableCell className="text-right">
