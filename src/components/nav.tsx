1e7af4c473a6ae27f4df84cb906ff335f560ad9e
Sandalu Thushan <sandaluthushan20@gmail.com>
2025-08-07 18:31:26 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <LinkComponent hre

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -35,7 +35,6 @@ const navItems = [
   { href: '/customers', label: 'Customer Management', icon: Users },
   { href: '/messages', label: 'Contact Messages', icon: Mail },
   { href: '/weddings', label: 'Wedding Management', icon: Heart },
-  { href: '/package-inclusions', label: 'Package Inclusions', icon: Gift },
   { href: '/website-content', label: 'Website Content', icon: Globe },
   { href: '/user-management', label: 'User Management', icon: UserCog },
   { href: '/api-example', label: 'API Example', icon: Terminal },
