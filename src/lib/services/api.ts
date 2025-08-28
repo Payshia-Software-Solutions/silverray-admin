887e5229e1de1289b44ad9718035c16327d6c303
Sandalu Thushan <sandaluthushan20@gmail.com>
2025-08-28 21:44:53 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/services/api.ts
+++ src/lib/services/api.ts
@@ -7,7 +7,7 @@
 
 // The base URL of your PHP server's router script
 const API_BASE_URL = 'https://silverray-server.payshia.com';
-export const CONTENT_PROVIDER_BASE_URL = 'https://content-provider.payshia.com/silver-ray/';
+export const CONTENT_PROVIDER_BASE_URL = 'https://content-provider.payshia.com/silver-ray';
 
 
 /**
