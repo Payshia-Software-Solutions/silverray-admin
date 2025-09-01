2c34f6f4e6af9cb8af99a95eb977126ddf8c88e9
Sandalu Thushan <sandaluthushan20@gmail.com>
2025-09-01 18:14:36 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         hostname: 'content-provider.payshia.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
