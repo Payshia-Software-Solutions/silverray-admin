dc837942dc552e1628b4a0678d992f1717288a2e
Sandalu Thushan <sandaluthushan20@gmail.com>
2025-10-02 12:09:11 +0000

    only this one blue color (_for element h1_)

--- src/app/(main)/header.tsx
+++ src/app/(main)/header.tsx
@@ -1,5 +1,4 @@
 
-
 'use client';
 
 import { usePathname } from 'next/navigation';
@@ -141,7 +140,7 @@ export function Header() {
 
   return (
     <div className="grid gap-1">
-      <h1 className="text-2xl md:text-3xl font-bold tracking-tight text-primary">{title}</h1>
+      <h1 className="text-2xl md:text-3xl font-bold tracking-tight text-blue-600">{title}</h1>
       <p className="text-muted-foreground">{description}</p>
     </div>
   );
